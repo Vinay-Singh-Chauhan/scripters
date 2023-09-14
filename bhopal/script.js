447b98a7af3f0dbc45126703a3689518eb0da544
Vinay Singh Chauhan <chauhansvinay@gmail.com>
2023-09-15 02:08:29 +0530

    added images

--- bhopal/script.js
+++ bhopal/script.js
@@ -25,3 +25,32 @@ container.addEventListener("mousemove", (e) => {
     const walk = (x - startX) * 2; // Adjust the multiplier for faster/slower scrolling
     container.scrollLeft = scrollLeft - walk;
 });
+
+const container2 = document.querySelector(".card-container2");
+
+let isDown1 = false;
+let startX1;
+let scrollLeft3;
+
+container2.addEventListener("mousedown", (e) => {
+    // console.log("heere")
+    isDown1 = true;
+    startX1 = e.pageX - container2.offsetLeft;
+    scrollLeft3 = container2.scrollLeft;
+});
+
+container2.addEventListener("mouseleave", () => {
+    isDown1 = false;
+});
+
+container2.addEventListener("mouseup", () => {
+    isDown1 = false;
+});
+
+container2.addEventListener("mousemove", (e) => {
+    if (!isDown1) return;
+    e.preventDefault();
+    const x = e.pageX - container2.offsetLeft;
+    const walk = (x - startX1) * 2; // Adjust the multiplier for faster/slower scrolling
+    container2.scrollLeft = scrollLeft3 - walk;
+});
